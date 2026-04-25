f6dc56ad758bcc813b0760231260cada4d9beadd
javadocq <han18264@naver.com>
2026-04-25 22:53:32 +0900

    feat: ProductRepository

---
+++ src/Infrastructure/Persistence/Repositories/LineRepository.cs
@@ -0,0 +1,19 @@
+﻿using LotTrace_MES.src.Domain.Entity;
+using LotTrace_MES.src.Domain.Enum;
+using LotTrace_MES.src.Domain.Interfaces;
+using LotTrace_MES.src.Infrastructure.Persistence.Repositories.Common;
+
+namespace LotTrace_MES.src.Infrastructure.Persistence.Repositories
+{
+    public class LineRepository : GenericRepository<Line>, ILineRepository
+    {
+        public LineRepository(AppDbContext context) : base(context)
+        {
+
+        }
+        public async Task<IEnumerable<Line>> GetByLineState(LineState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using LotTrace_MES.Domain.Interfaces;
+using LotTrace_MES.src.Domain.Entity;
+using LotTrace_MES.src.Domain.Interfaces.Common;
+using LotTrace_MES.src.Infrastructure.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotTrace_MES.src.Infrastructure.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context) {
+            }
+
+        public async Task<Product?> GetProductCodeAsync(string productCode)
+        {
+            return await _dbSet.FirstOrDefaultAsync(p => p.ProductCode == productCode);
+        }
+    }
+}
