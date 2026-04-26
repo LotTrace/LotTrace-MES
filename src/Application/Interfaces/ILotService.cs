c988f99b8734165effbfbe8c99cc0b34ccf13b5a
javadocq <han18264@naver.com>
2026-04-26 15:35:43 +0900

    feat: ILineService

--- src/Application/Interfaces/ILotService.cs
+++ src/Application/Interfaces/ILotService.cs
@@ -6,7 +6,7 @@ namespace LotTrace_MES.src.Application.Interfaces
     public interface ILotService
     {
         Task<bool> CreateLotAsync(string barcode, int productId, int workerId, int lineId); // Lot 생성
-        Task<bool> MoveNextStepAsync(int barcode, int workerId); // Lot의 다음 단계로 이동
-        Task<bool> ChangedStateAsync(int barcode, LotState newState, int workerId); // Lot 상태 변경
+        Task<bool> MoveNextStepAsync(string barcode, int workerId); // Lot의 다음 단계로 이동
+        Task<bool> ChangedStateAsync(string barcode, LotState newState, int workerId); // Lot 상태 변경
     }
 }
