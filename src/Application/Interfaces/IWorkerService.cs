1261c97e19900aba274552afb731ffbccaf84443
javadocq <han18264@naver.com>
2026-04-26 16:16:51 +0900

    feat: IWorkerService

---
+++ src/Application/Interfaces/IWorkerService.cs
@@ -0,0 +1,13 @@
+﻿using LotTrace_MES.src.Domain.Entity;
+
+namespace LotTrace_MES.src.Application.Interfaces
+{
+    public interface IWorkerService
+    {
+        Task<IEnumerable<Worker>> GetAllWorkerAsync(); // 모든 작업자 조회
+        Task<IEnumerable<Worker>> GetWorkersByDepartmentAsync(string department); // 부서별 작업자 조회
+        Task<Worker?> GetWorkerByNameAsync(string name); // 이름으로 작업자 조회
+        Task<Worker?> GetWorkerByEmployeeNumberAsync(int employeeNumber); // 사번으로 작업자 조회
+        Task<Worker> CreateWorkerAsync(int employeeNumber, string name, string deparment); // 작업자 생성
+    }
+}
