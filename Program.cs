261ecad22ec35010ba9891a8a8210a818e7a442c
javadocq <han18264@naver.com>
2026-04-29 18:53:37 +0900

    feat: Dependency-inject

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using LotTrace_MES.Domain.Interfaces;
+using LotTrace_MES.src.Application.Interfaces;
+using LotTrace_MES.src.Application.Service;
+using LotTrace_MES.src.Domain.Interfaces;
 using LotTrace_MES.src.Infrastructure;
+using LotTrace_MES.src.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace LotTrace_MES
@@ -11,6 +16,21 @@ namespace LotTrace_MES
             // DB 설정
             builder.Services.AddDbContext<AppDbContext>(opt =>
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Repository DI 등록
+            builder.Services.AddScoped<ILineRepository, LineRepository>();
+            builder.Services.AddScoped<IWorkerRepository, WorkerRepository>();
+            builder.Services.AddScoped<ILotRepository, LotRepository>();
+            builder.Services.AddScoped<ILogHistoriesRepository, LogHistoriesRepository>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Service DI 등록
+            builder.Services.AddScoped<ILineService, LineService>();
+            builder.Services.AddScoped<IWorkerService, WorkerService>();
+            builder.Services.AddScoped<ILogHistoriesService, LogHistoriesService>();
+            builder.Services.AddScoped<ILotService, LotService>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
