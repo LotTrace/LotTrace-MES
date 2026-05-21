8669574bc3814189a49236f62c3f7b3c0bd3e9ed
javadocq <han18264@naver.com>
2026-05-21 18:13:40 +0900

    feat: LotStateTransition Domain

---
+++ src/Domain/Entity/LotStateTransition.cs
@@ -0,0 +1,13 @@
+﻿using LotTrace_MES.src.Domain.Enum;
+
+namespace LotTrace_MES.src.Domain.Entity
+{
+    public class LotStateTransition
+    {
+        public int Id { get; set; }
+        public LotState FromState { get; set; }
+        public LotState ToState { get; set; }
+        public bool IsAllowed { get; set; }
+        public string? Description { get; set; }
+    }
+}
