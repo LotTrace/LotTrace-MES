3d665db4e226339ef9c1bc2fb53505e8f49c884a
javadocq <han18264@naver.com>
2026-05-30 05:45:39 +0900

    feat: Order Interface

---
+++ src/Application/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using LotTrace_MES.src.Domain.Entity;
+
+namespace LotTrace_MES.src.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(int productId, int quantity); 
+        Task<bool> StartOrderAsync(int orderId);
+        Task<bool> CompleteOrderAsync(int orderId);
+        Task<bool> UpdateProductQtyAsync(int orderId, int newQuantity);
+    }
+}
