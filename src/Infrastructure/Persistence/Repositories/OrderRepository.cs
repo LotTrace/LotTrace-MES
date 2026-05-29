94d764e8989a3f0c4cff02f9bb833d72bac936a4
javadocq <han18264@naver.com>
2026-05-30 05:31:23 +0900

    feat: OrderRepository

---
+++ src/Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using LotTrace_MES.src.Domain.Entity;
+using LotTrace_MES.src.Infrastructure.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotTrace_MES.src.Infrastructure.Persistence.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>
+    {
+        public OrderRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersWithProductAsync()
+        {
+            return await _dbSet.Include(o => o.Product).OrderByDescending(o => o.OrderId).ToListAsync();
+        }
+    }
+}
