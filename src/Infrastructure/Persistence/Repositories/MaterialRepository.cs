dfb87c8f589f51d61de8a948a4f186d1aa150c70
javadocq <han18264@naver.com>
2026-05-30 05:37:09 +0900

    feat: MaterialRepository

---
+++ src/Infrastructure/Persistence/Repositories/MaterialRepository.cs
@@ -0,0 +1,18 @@
+﻿using LotTrace_MES.src.Domain.Entity;
+using LotTrace_MES.src.Infrastructure.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotTrace_MES.src.Infrastructure.Persistence.Repositories
+{
+    public class MaterialRepository : GenericRepository<Material>
+    {
+        public MaterialRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Material>> GetLowStockMaterialAsync(int threshold = 10)
+        {
+            return await _dbSet.Where(m => m.Stock <= threshold).OrderBy(m => m.Stock).ToListAsync();
+        }
+    }
+}
